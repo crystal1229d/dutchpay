33b0213b241a126497700d94217d36cfaea2d18a
crystal1229d <crystal1229d@naver.com>
2024-01-10 00:03:28 +0900

    그룹 멤버 추가 > 삼성 인터넷일 경우 input tags 미사용

--- src/components/AddMembers.jsx
+++ src/components/AddMembers.jsx
@@ -7,11 +7,13 @@ import styled from 'styled-components';
 import { CenteredOverlayForm } from './CenteredOverlayForm';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '../routes';
+import { Form } from 'react-bootstrap';
 
 export const AddMembers = () => {
     const [validated, setValidated] = useState(false);
     const [grouopMembers, setGroupMembers] = useRecoilState(groupMembersState);
     const groupName = useRecoilValue(groupNameState); // value 만 가져오기
+    const [groupMembersString, setGroupMembersString] = useState('')
 
     const navigate = useNavigate();
 
@@ -21,8 +23,21 @@ export const AddMembers = () => {
 
         if (grouopMembers.length > 0) {
             navigate(ROUTES.EXPENSES_MAIN)
+        } else {
+            if (groupMembersString.length > 0) {
+                // 1. 태그가 동작하지 않았을 때
+                setGroupMembers(groupMembersString.split(','))
+            }
+            // 2. 사용자가 아무런 멤버도 입력하지 않았을 때
         }
     }
+
+    // TODO: Performance Optimization 
+    // IIFE (즉시실행함수) => 일반함수로 선언 후 return 문 내에서 실행 시, 4번 호출됨 
+    // 4번이나 호출할 필요가 없으므로 return 문 전에 즉시실행하여 한 번만 실행하고 그 값을 저장하여 return 문 내에서 이용 
+    const isSamsungInternet = (() => {
+        return window.navigator.userAgent.includes('SAMSUNG')
+    })()
 
     return (
         <CenteredOverlayForm
@@ -30,10 +45,20 @@ export const AddMembers = () => {
             validated={validated}
             handleSubmit={handleSubmit}
         >
-            <InputTags
-                placeholder='이름 간 띄어 쓰기'
-                onTags={(value) => setGroupMembers(value.values)}
-            /> 
+
+            {/* TODO: InputTags가 동작하지 않는 환경에서, 이름 값을 ,를 구분자로 해서 받는다 */}    
+            {
+                isSamsungInternet
+                    ? (<Form.Control 
+                            placeholder='이름 간 컴마(,)로 구분'
+                            onChange={({target}) => setGroupMembersString(target.value)}
+                        />)
+                    : (<InputTags
+                            data-testid='input-member-names'
+                            placeholder='이름 간 띄어 쓰기'
+                            onTags={(value) => setGroupMembers(value.values)}
+                        />)
+            }
             {
                 validated && grouopMembers.length === 0
                 && (<StyledErrorMessage>그룹 멤버들의 이름을 입력해주세요.</StyledErrorMessage>)
